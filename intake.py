2d6a536f9dd5d1ec7b01354af78195927129d901
John Ragland <jhrag@uw.edu>
2023-05-04 16:18:20 +0000

    added coordinates to DAS

--- intake.py
+++ intake.py
@@ -6,7 +6,7 @@ March 2023
 '''
 import xarray as xr
 import os
-
+import pandas as pd
 
 def open_ooi_lfhydrophones():
     '''
@@ -25,3 +25,13 @@ def open_ooi_DAS_SouthTx():
     storage_options = {'account_name':'dasdata', 'account_key':os.environ['AZURE_KEY_dasdata']}
     ds = xr.open_zarr('abfs://zarr/ooi_South_Tx.zarr/ooi_South_Tx.zarr', storage_options=storage_options)
     return ds
+
+def open_ooi_DAS_SouthTx_RawData(geo_fn = '/datadrive/DAS/south_DAS_latlondepth.txt'):
+    ds = open_ooi_DAS_SouthTx()
+    geo = pd.read_csv(geo_fn)
+    geo['distance'] = geo['index']*2.0419046878814697/1000
+
+    ds_slice = ds.isel({'distance':slice(942, 47382)})
+    ds = ds_slice.assign_coords({'distance':geo['distance'].values})
+
+    return ds, geo
